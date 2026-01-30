ddf67f87c1a2c6f2f53e5bf9b31eab617742225f
muluneshtesfa2006-dev <muluneshtesfa2006@gmail.com>
2026-01-30 17:12:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // This allows the tablet to connect to your PC
-    port: 5173
-  }
+  base: "/lifeos/",
 });
